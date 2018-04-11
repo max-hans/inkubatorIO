7ba96aca495322fa36c2b1b4ffbcd21eb9e951ca
Maximilian Hans <m.hans@me.com>
2018-04-11 12:53:23 +0200

    Update ConstantSpeed1Stepper.ino

--- Code/Start/constantSpeed1Stepper/ConstantSpeed1Stepper.ino
+++ Code/Start/constantSpeed1Stepper/ConstantSpeed1Stepper.ino
@@ -5,10 +5,10 @@ AccelStepper stepper1(8, 5, 7, 6, 8);
 // schema : name(8 <- bezeichnet den steppertyp , pin1 , pin 3 , pin 2 , pin 4)
 // ! achtung ! pin 2 und pin 3 müssen vertauscht werden !
 
-
 int stepperSpeed = 100;
 
 void setup() {
+  stepper1.setMaxSpeed(stepperSpeed);
   stepper1.setSpeed(stepperSpeed);
 }
 
